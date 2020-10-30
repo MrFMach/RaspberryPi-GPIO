c43466076a2aec181ad52d15060b72b5ba1c7867
MrFMach <fabiosilvamachado@gmail.com>
2020-10-30 20:29:01 -0300

    update files

--- gpio.py
+++ gpio.py
@@ -1,57 +1,59 @@
-
 import RPi.GPIO as gpio
 import time
-
-# Todo o código no bloco try para tratamento de erros
-try:
-    
-    # Desativa os avisos da GPIO
+# All code in the try block for error handling
+try:    
+    # Disable GPIO warnings
     gpio.setwarnings(False)
-    # Referecia pela pinagem da placa (1-40)
+    # Pins board reference (1-40)
     gpio.setmode(gpio.BOARD)
-    # Configuração das saídas
+    # Set output
+    gpio.setup(23, gpio.OUT, initial=gpio.LOW)
     gpio.setup(31, gpio.OUT, initial=gpio.LOW)
     gpio.setup(33, gpio.OUT, initial=gpio.LOW)
     gpio.setup(35, gpio.OUT, initial=gpio.LOW)
     gpio.setup(37, gpio.OUT, initial=gpio.LOW)
-    # Configuração das entradas
+    # Set input
     gpio.setup(3, gpio.IN)
     gpio.setup(5, gpio.IN)
     gpio.setup(7, gpio.IN)
-
-
+    
+    # Started signal
+    gpio.output(23, 1)
+    
     while 1:
-        
+
         if gpio.input(3) == False:
-            print("Botão 3 pressionado")
+            print("Button-3 pressed, blinking Led-33")
             gpio.output(33, 1)
             time.sleep(0.1)
             gpio.output(33, 0)
             time.sleep(0.1)
 
         elif gpio.input(5) == False:
-            print("Botão 5 pressionado")
+            print("Button-5 pressed, blinking Led-37")
             gpio.output(37, 1)
             time.sleep(0.1)
             gpio.output(37, 0)
             time.sleep(0.1)
      
         elif gpio.input(7) == False:
-            print("Botão 7 pressionado")
+            print("Button-7 pressed, blinking Led-35")
             gpio.output(35, 1)
             time.sleep(0.1)
             gpio.output(35, 0)
             time.sleep(0.1)
         else:
-            print("Nenhum botão pressionado")
+            print("No button pressed")
+            print("-press any button or type Ctrl+C to exit\n")
             gpio.output(31, 1)
             time.sleep(0.3)
             gpio.output(31, 0)
-            time.sleep(0.3)
+            time.sleep(0.5)
 
-# Se pressionar Ctrl+C, executa o que está no 'except' e vai para 'finally'
-except KeyboardInterrupt:
-    print ("Você saiu do programa")
-    
+# If you press Ctrl+C, execute what is in 'except' and go to 'finally'
+except KeyboardInterrupt:    
+    print ("\nYou left the program")    
 finally:
+    print ("Cleaning up GPIO...")
     gpio.cleanup()
+    print ("Bye!")
